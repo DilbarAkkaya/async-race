d57df543ff8c74db2aa059a7beda615063acac69
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-01-10 23:39:35 +0300

    feat: set config webpack, create style.scss, create folder oublic, assets, index.html

--- async-race/webpack.config.js
+++ async-race/webpack.config.js
@@ -1,9 +1,62 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.js',
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    hot: true,
+    port: 8080,
+    static: path.join(__dirname, 'public'),
+  }
+};
+module.exports = ({develop}) => ({
+  mode: develop ? 'development' : 'production',
+  devtool: develop ? 'inline-source-map' : 'none',
+  entry: {
+    app: './src/index.js',
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
   },
-};
+  module: {
+  rules: [
+    {
+      test: /\.(?:ico|gif|jpg|jpeg|svg)$/i,
+      type: 'asset/resource',
+    },
+    {
+      test: /\.(woff(2)?|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    },
+    {
+      test: /\.css$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader']
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+    }
+  ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './public'}]
+    }),
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+  ],
+  ...devServer(develop)
+});
