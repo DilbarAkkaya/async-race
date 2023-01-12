05c75a34a2eb75759576e02ba8a51988e8026f1f
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-01-13 01:53:54 +0300

    refactor: comment mainpagecreate in garagepage

--- async-race/src/pages/garage.js
+++ async-race/src/pages/garage.js
@@ -1,7 +1,7 @@
 import { createLinkElement } from '../components/link';
 import { createHeader } from '../components/header';
-import { createNewElement } from '../utils';
-import { createGarageMain } from './garageMain';
+// import { createNewElement } from '../utils';
+// import { createGarageMain } from './garageMain';
 import { route } from '../router/router';
 
 export const toGarageButton = createLinkElement({ class: 'btn btn-primary', href: '/' }, 'To Garage');
@@ -10,13 +10,12 @@ export const toWinnerButton = createLinkElement({ class: 'btn btn-primary', href
 export function renderGarageView() {
   const { body } = document;
   const header = createHeader();
-  const main = createGarageMain();
-  const mainPage = createNewElement('body', 'div', { class: 'main-page', id: 'main-page' });
+  // const main = createGarageMain();
+  // const mainPage = createNewElement('body', 'div', { class: 'main-page', id: 'main-page' });
   header.append(toGarageButton);
   header.append(toWinnerButton);
   body.prepend(header);
-  mainPage.append(main);
-
+  // mainPage.append(main);
   // body.append(garagePage);
 }
 
