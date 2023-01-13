e8aff8297caf408371e9df80d4380b4f13bc6e8a
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-01-14 01:39:15 +0300

    refactor: delete comments

--- async-race/src/pages/garage/garage.js
+++ async-race/src/pages/garage/garage.js
@@ -1,7 +1,5 @@
 import { createLinkElement } from '../../components/link';
 import { createHeader } from '../../components/header';
-// import { createNewElement } from '../utils';
-// import { createGarageMain } from './garageMain';
 import { route } from '../../router/router';
 
 const garageButton = createLinkElement({ class: 'btn btn-primary', href: '/' }, 'To Garage');
@@ -10,13 +8,9 @@ const winnerButton = createLinkElement({ class: 'btn btn-primary', href: '/winne
 export function renderGarageView() {
   const { body } = document;
   const header = createHeader();
-  // const main = createGarageMain();
-  // const mainPage = createNewElement('body', 'div', { class: 'main-page', id: 'main-page' });
   header.append(garageButton);
   header.append(winnerButton);
   body.prepend(header);
-  // mainPage.append(main);
-  // body.append(garagePage);
 }
 
 garageButton.addEventListener('click', route);
