3fb2a4c16916a13b4f65fb723ecc6bba08f125bc
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-01-14 02:47:05 +0300

    feat: install normalize.css, add styles for main, header, buttons

--- async-race/src/components/header.js
+++ async-race/src/components/header.js
@@ -1,7 +1,7 @@
 export function createHeader() {
   const fragment = new DocumentFragment();
   const header = document.createElement('header');
-  header.className = 'header';
+  header.className = 'header row';
   fragment.append(header);
   return header;
 }
--- async-race/src/pages/garage/garageMain.js
+++ async-race/src/pages/garage/garageMain.js
@@ -5,18 +5,18 @@ import { renderForm } from '../../components/form';
 function createGarageMain() {
   const parent = new DocumentFragment();
   const main = document.createElement('main');
-  main.className = 'main';
+  main.className = 'main column';
   parent.append(main);
   return main;
 }
 export function renderGarageMain() {
   const main = createGarageMain();
-  const carCreateForm = renderForm('form-create', 'name-create', 'text', 'CREATE');
-  const carUpdateForm = renderForm('form-update', 'name-update', 'text', 'UPDATE');
+  const carCreateForm = renderForm('form-create', 'name-create', 'text', 'create');
+  const carUpdateForm = renderForm('form-update', 'name-update', 'text', 'update');
   const containerButtons = createNewElement('div', { class: 'btn-container' });
-  const raceButton = createButtonElement({ class: 'btn btn-ptimary' }, 'RACE');
-  const resetButton = createButtonElement({ class: 'btn btn-ptimary' }, 'RESET');
-  const generateCarsButton = createButtonElement({ class: 'btn btn-ptimary' }, 'GENERATE CARS');
+  const raceButton = createButtonElement({ class: 'btn btn-ptimary' }, 'race');
+  const resetButton = createButtonElement({ class: 'btn btn-ptimary' }, 'reset');
+  const generateCarsButton = createButtonElement({ class: 'btn btn-ptimary' }, 'generate cars');
   const titleGarage = createNewElement('h1', { class: 'title' }, 'GARAGE  ');
   const countCars = createNewElement('span', { class: 'count' });
   main.prepend(carCreateForm);
