01ea49ac89d2f8e0f6d2b47d9d0b13e816e70790
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-01-13 23:54:03 +0300

    feat: create constats.js for variables, change atrtibute value in input color

--- async-race/src/components/form.js
+++ async-race/src/components/form.js
@@ -1,12 +1,13 @@
 import { createNewElement } from '../utils';
 import { createInput } from './input';
+import { COLORS } from '../constants';
 
 export function createForm(arg) {
   const parent = new DocumentFragment();
   const form = createNewElement('form', { class: arg, id: arg });
   const input = createInput('name-create', 'text');
   const inputColor = createInput('color', 'color');
-  inputColor.setAttribute('value', '#ffffff');
+  inputColor.setAttribute('value', COLORS.white);
   form.append(input);
   form.append(inputColor);
   parent.append(form);
