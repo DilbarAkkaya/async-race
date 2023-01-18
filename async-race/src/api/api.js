4cd277ca09110f770b37cfb77f237c917ad480a5
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-01-19 02:08:31 +0300

    refactor: delete func savecarsandcount, result in getcars, write data on store directly

--- async-race/src/api/api.js
+++ async-race/src/api/api.js
@@ -1,15 +1,16 @@
 import { url } from '../constants';
+import { store } from '../store';
 
 export async function getCars(page = 1, limitOfCarsOnPage = 7) {
-  const result = {
+/*   const result = {
     items: [],
     count: '',
-  };
+  }; */
   const response = await fetch(`${url}?_page${page}&_limit=${limitOfCarsOnPage}`);
   if (response.ok) {
-    result.items = await response.json();
-    result.count = response.headers.get('X-Total-Count');
-    return result;
+    store.dataApi.items = await response.json();
+    store.dataApi.count = response.headers.get('X-Total-Count');
+    return store;
   }
   throw new Error(`Could not fetch ${url}, status: ${response.status}`);
 }
--- async-race/src/pages/garage/listOfCars.js
+++ async-race/src/pages/garage/listOfCars.js
@@ -2,14 +2,15 @@ import { getCars } from '../../api/api';
 import { Car } from './classCar';
 import { store } from '../../store';
 
-async function saveFetchCarsAndCountToStore() {
+/* async function saveFetchCarsAndCountToStore() {
   const res = await getCars();
   store.dataApi.items = res.items;
   store.dataApi.count = res.count;
-}
+} */
 
 export async function renderCarsAndCount(parentSelector) {
-  await saveFetchCarsAndCountToStore();
+  await getCars();
+ // await saveFetchCarsAndCountToStore();
   store.dataApi.items.forEach((item) => new Car(item.name, item.id, item.color, parentSelector).renderCar());
   const countCars = document.querySelector('.count');
   countCars.innerHTML = store.dataApi.count;
