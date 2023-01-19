aa6df373e0a96ebf4b631e9eeeff538f476a2087
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-01-19 03:20:30 +0300

    refactor: replace and rename file garage.js to commonviewpae

--- async-race/src/index.js
+++ async-race/src/index.js
@@ -1,5 +1,5 @@
 import './style.scss';
-import { renderCommonView } from './pages/garage/garage';
+import { renderCommonView } from './pages/commonViewPage';
 
 document.addEventListener('DOMContentLoaded', async () => {
   renderCommonView();
--- async-race/src/pages/commonViewPage.js
+++ async-race/src/pages/commonViewPage.js
@@ -1,6 +1,6 @@
-import { createLinkElement } from '../../components/link';
-import { createHeader } from '../../components/header';
-import { route } from '../../router/router';
+import { createLinkElement } from '../components/link';
+import { createHeader } from '../components/header';
+import { route } from '../router/router';
 
 const garageButton = createLinkElement({ class: 'btn btn-primary', href: '/' }, 'To Garage');
 const winnerButton = createLinkElement({ class: 'btn btn-primary', href: '/winners' }, 'To Winners');
