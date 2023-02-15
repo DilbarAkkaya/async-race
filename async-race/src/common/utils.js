5d67b3bb9eb9e3cfa42fd773cb7896b06ae7d5a3
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-15 16:22:00 +0300

    refactor: change func createnewel with switch case

--- async-race/src/common/utils.js
+++ async-race/src/common/utils.js
@@ -6,13 +6,21 @@ export function createNewElement(el, attrs, text) {
   const newElement = document.createElement(el);
   if (attrs) {
     Object.keys(attrs).forEach((key) => {
-      if (key === 'class') {
+      switch (key) {
+        case ('class'): newElement.className = attrs[key];
+          break;
+        case ('id'): newElement.id = attrs[key];
+          break;
+        default: newElement.setAttribute(key, attrs[key]);
+          break;
+      }
+/*       if (key === 'class') {
         newElement.className = attrs[key];
       } else if (key === 'id') {
         newElement.id = attrs[key];
       } else {
         newElement.setAttribute(key, attrs[key]);
-      }
+      } */
     });
   }
   if (text) {
